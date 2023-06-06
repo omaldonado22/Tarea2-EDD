113290d94b5c8ca6ff8eb1491011eb22e6b2b570
omaldonado22 <orlynm10@gmail.com>
2023-06-06 10:08:51 -0600

    Cambios

--- Ejercicio1.cpp
+++ Ejercicio1.cpp
@@ -1,26 +1,27 @@
 #include <iostream>
-
+using namespace std;
 int main() {
-    const int size = 8;
-    int arr[size];
+
+    int arreglo[8];
 
     // Ingresar los elementos por teclado
-    std::cout << "Ingrese " << size << " elementos:\n";
-    for (int i = 0; i < size; i++) {
-        std::cout << "Elemento " << i + 1 << ": ";
-        std::cin >> arr[i];
+   
+    for (int i = 0; i < 8; i++) {
+       cout << "Elemento " << i + 1 << ": ";
+        cin >> arreglo[i];
+          cout << "Ingrese " << i << " elementos:\n";
     }
 
     // Ordenar los elementos mediante búsqueda exhaustiva ssooo
     bool intercambio = true;
     while (intercambio) {
         intercambio = false;
-        for (int i = 0; i < size - 1; i++) {
-            if (arr[i] > arr[i + 1]) {
+        for (int i = 0; i < 8 - 1; i++) {
+            if (arreglo[i] > arreglo[i + 1]) {
                 // Intercambiar los elementos
-                int temp = arr[i];
-                arr[i] = arr[i + 1];
-                arr[i + 1] = temp;
+                int temp = arreglo[i];
+                arreglo[i] = arreglo[i + 1];
+                arreglo[i + 1] = temp;
                 intercambio = true;
             }
         }
@@ -28,22 +29,22 @@ int main() {
 
     // Sumar los elementos del arreglo ordenado
     int suma = 0;
-    for (int i = 0; i < size; i++) {
-        suma += arr[i];
+    for (int i = 0; i < 8; i++) {
+        suma += arreglo[i];
     }
 
     // Imprimir la suma total, el arreglo original y el arreglo ordenado
     std::cout << "Suma total de los elementos del arreglo ordenado: " << suma << std::endl;
 
     std::cout << "Arreglo original: ";
-    for (int i = 0; i < size; i++) {
-        std::cout << arr[i] << " ";
+    for (int i = 0; i < 8; i++) {
+        std::cout << arreglo[i] << " ";
     }
     std::cout << std::endl;
 
     std::cout << "Arreglo ordenado: ";
-    for (int i = 0; i < size; i++) {
-        std::cout << arr[i] << " ";
+    for (int i = 0; i < 8; i++) {
+        std::cout << arreglo[i] << " ";
     }
     std::cout << std::endl;
 
